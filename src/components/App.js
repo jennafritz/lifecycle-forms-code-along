87f127f8eaa3713836dddb9083959d28a4b8e881
jennafritz <jennafritz24@gmail.com>
2021-04-29 11:20:53 -0400

    Lecture complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import pokeData from '../data/pokemon'
+//no longer using this, but good to test with static data first, get it working, then replace with API data:
+// import pokeData from '../data/pokemon'
 
 //Components
 import Navbar  from './Navbar'
@@ -8,10 +9,22 @@ import Home from './Home'
 
 export default class App extends Component {
 
+  constructor(){
+    super()
+    console.warn("running constructor...")
+  }
+
   state = {
     display: "Home",
     searchText: "",
-    pokemons: pokeData
+    pokemons: []
+  }
+
+  componentDidMount() {
+    console.warn("running didMount...")
+    fetch('http://localhost:3000/pokemon')
+      .then(res => res.json())
+      .then(pokeData => this.setState({pokemons: pokeData}))
   }
 
   changeToPokemon = () => {
@@ -28,6 +41,10 @@ export default class App extends Component {
     })
   }
 
+  //this returns an array with all pokemon objects except the one with an id equal to the id of the passed-in pokemon object
+  //it is passed down to PokeContainer as a prop from App (PokeContainer is the parent of Card)
+  //then passed down to Card as a prop from parent PokeContainer
+  //this function is then called on onClick in the Card component from its props
   deletePokemon = (pokemonObj) => {
     const newPokemon= this.state.pokemons.filter(pokemon=> pokemon.id !== pokemonObj.id)
     console.log("hey");
@@ -37,14 +54,23 @@ export default class App extends Component {
 
   }
 
+  //best to define this in App because the state we want to change (the pokemons array) is in this component
+  //pass down as prop to PokeContainer
+  //then pass down as another prop from PokeContainer to Form and invoke in the POST request within Form
+  createPokemon = (pokemonObj) => {
+    this.setState({
+      pokemons: [pokemonObj, ...this.state.pokemons]
+    })
+  }
 
   render(){
+    console.warn("running render...")
    const filteredPokemon = this.state.pokemons.filter(poke => poke.name.includes(this.state.searchText))
     return (
       <div className="bg-dark">
         <Navbar handleSearchText={this.handleSearchText} display={this.state.display} changeToHome={this.changeToHome} />
         { this.state.display === "Home" ? <Home changeToPokemon={this.changeToPokemon}/> : null }
-        { this.state.display === "Pokemon" ? <PokeContainer deletePokemon={this.deletePokemon} pokemons={filteredPokemon}  /> : null}
+        { this.state.display === "Pokemon" ? <PokeContainer createPokemon={this.createPokemon} deletePokemon={this.deletePokemon} pokemons={filteredPokemon}  /> : null}
       </div>
     )
   }
--- src/components/PokeContainer.js
+++ src/components/PokeContainer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-
 //Components
 import { Card } from './Card'
+import Form from './Form'
 
 export default class PokeContainer extends React.Component {
 
@@ -12,6 +12,7 @@ export default class PokeContainer extends React.Component {
 
         return(
             <>
+            <Form createPokemon={this.props.createPokemon}/>
             <div className="row ml-3">
               { this.props.pokemons.map(pokemon => <Card deletePokemon={this.props.deletePokemon} key={pokemon.id} pokemon={pokemon}/>) }
             </div>
